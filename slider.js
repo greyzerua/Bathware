40ab2f6477acf9df48d68fd1ce433df61c2efd82
greyzerUA <greyzerua@ukr.net>
2023-06-14 12:03:04 +0300

    Add new blocks

--- slider.js
+++ slider.js
@@ -6,3 +6,30 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
       }
     });
+
+var swiper = new Swiper(".blogs-slider", {
+      slidesPerView: 3,
+      loop: true,
+      centerInsufficientSlides: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+      // when window width is >= 320px
+      800: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+      500: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      
+  }
+    });
